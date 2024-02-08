0db3b2292f4c3587c435acc61717b4f07568c46d
TAKUYA KOIDE <takuya.koide@konicaminolta.com>
2024-02-08 14:03:07 +0900

    トレーナーページの追加と削除機能の実装

---
+++ pages/trainer/[name]/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+const config = useRuntimeConfig();
+const {data: trainer, refresh} = await useFetch(
+  () => `/api/trainer/${route.params.name}`,
+  {
+    default: () => [],
+    baseUrl: config.public.backendOrigin,
+  },
+);
+const onDelete = async () => {
+  const response = await $fetch(`/api/trainer/${route.params.name}`, {
+    baseURL: config.public.backendOrigin,
+    method: "DELETE",
+  }).catch((e) => e);
+  if (response instanceof Error) return;
+  router.push("/");
+};
+
+
+
+
+
+</script>
