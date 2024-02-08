189e833b8179c0b306e0abb667c5af24971b48ec
TAKUYA KOIDE <takuya.koide@konicaminolta.com>
2024-02-08 14:04:35 +0900

    ページのトレーナー/[名前]/catch.vueを追加しました

---
+++ pages/trainer/[name]/catch.vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+const config = useRuntimeConfig();
+const page = ref(0);
+const limit = ref(20);
+const offset = computed(() => page.value * limit.value);
+
+
+
+</script>
