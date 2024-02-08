ff68e239f7f9021566371c09cd1a1a917bee1ac5
TAKUYA KOIDE <takuya.koide@konicaminolta.com>
2024-02-08 15:37:01 +0900

    ローカル開発環境でのエラー修正***

--- server/utils/router.js
+++ server/utils/router.js
@@ -79,8 +79,7 @@ router.delete("/trainer/:trainerName", async (req, res, next) => {
 });
 
 /** ポケモンの追加 */
-
-// router.post
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // Local開発環境でこれを追加しないとエラーが出る
 router.put("/trainer/:trainerName/pokemon", async (req, res, next) => {
     try {
       const {trainerName, pokemonName} = req.params;
