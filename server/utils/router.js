4b17d6e21847c109c927bdca7b91313bc7e5d836
TAKUYA KOIDE <takuya.koide@konicaminolta.com>
2024-02-08 13:12:56 +0900

    ポケモンの追加を修正

--- server/utils/router.js
+++ server/utils/router.js
@@ -82,11 +82,28 @@ router.delete("/trainer/:trainerName", async (req, res, next) => {
 
 
 /** ポケモンの追加 */
-router.post("/trainer/:trainerName/pokemon", async (req, res, next) => {
+
+// router.post
+router.put("/trainer/:trainerName/pokemon", async (req, res, next) => {
   try {
-    const { trainerName } = req.params;
+    const {trainerName, pokemonName} = req.params;
     // TODO: リクエストボディにポケモン名が含まれていなければ400を返す
-    const pokemon = await findPokemon(req.body.name);
+    const pokemon = await findPokemon(pokemonName);
+    const trainer = await findTrainer(trainerName);
+    const {
+      order, name,
+      sprites: { front_default },
+    } = pokemon;
+    trainer.pokemons.push(
+      id: (trainer.pokemons[trainer.pokemons.length - 1].id + 1)
+      +1,
+      nickname:"",
+      order,
+      name,
+      sprites:{front_default},
+    });
+
+    
     // TODO: 削除系 API エンドポイントを利用しないかぎりポケモンは保持する
     const result = await upsertTrainer(trainerName, { pokemons: [pokemon] });
     res.status(result["$metadata"].httpStatusCode).send(result);
