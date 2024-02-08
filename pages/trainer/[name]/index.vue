d58212c4369d8d0e4d28f9387afec1c41ca54e49
TAKUYA KOIDE <takuya.koide@konicaminolta.com>
2024-02-08 14:03:36 +0900

    トレーナーページの修正

--- pages/trainer/[name]/index.vue
+++ pages/trainer/[name]/index.vue
@@ -17,9 +17,49 @@ const onDelete = async () => {
   if (response instanceof Error) return;
   router.push("/");
 };
-
-
-
-
-
+const nickname = ref("");
+const onNickname = async (pokemon) => {
+  const newTrainer = trainer.value;
+  const index = newTrainer.pokemons.findIndex(({ id }) => id === pokemon.id);
+  newTrainer.pokemons[index].nickname = trimAvoidCharacters(nickname.value);
+  nickname.value = "";
+  const response = await $fetch(`/api/trainer/${route.params.name}`, {
+    baseURL: config.public.backendOrigin,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTrainer),
+  }).catch((e) => e);
+  if (response instanceof Error) return;
+  await refresh();
+  onCloseNickname();
+};
+const onRelease = async (pokemonId) => {
+  const response = await fetch(
+    `/api/trainer/${route.params.name}/pokemon/${pokemonId}`,
+    {
+      baseURL: config.public.backendOrigin,
+      method: "DELETE",
+    },
+  ).catch((e) => e);
+  if (response instanceof Error) return;
+  await refresh();
+  onCloseRelease();
+};
+const {
+  dialog: deleteDialog,
+  onOpen: onOpenDelete,
+  onClose: onCloseDelete,
+} = useDialog();
+const {
+  dialog: nicknameDialog,
+  onOpen: onOpenNickname,
+  onClose: onCloseNickname,
+} = useDialog();
+const {
+  dialog: releaseDialog,
+  onOpen: onOpenRelease,
+  onClose: onCloseRelease,
+} = useDialog();
 </script>
